901bf3310da9a8b7d9257bbe42d2b6ca2ac27c04
caleberi <caleberioluwa@gmail.com>
2022-07-10 21:08:00 +0100

    feat:  add CORS

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 
 from db import database
 from resources.routes import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(api_router)
 
